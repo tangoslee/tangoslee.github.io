ca3e23aed70ba8bba88ba389f1c06541867169cf
Sangmin Lee <tangoslee@gmail.com>
2018-10-27 18:23:27 -0400

    Auto-generated commit

--- assets/js/agency.js
+++ assets/js/agency.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +16,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -41,11 +41,34 @@
   // $(window).scroll(navbarCollapse);
 
   // Hide navbar when modals trigger
-  $('.portfolio-modal').on('show.bs.modal', function(e) {
+  $('.portfolio-modal').on('show.bs.modal', function (e) {
     $(".navbar").addClass("d-none");
   })
-  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+  $('.portfolio-modal').on('hidden.bs.modal', function (e) {
     $(".navbar").removeClass("d-none");
   })
+
+
+  $(document).on('click', '.btn-go', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var $this = $(this);
+    var isNext = ($this.attr('class').match(/carousel-control-next/)) ? true : false;
+    var items = $this.parent().find('.carousel-item');
+    var index = -1;
+    items.each(function (i, item) {
+      if ($(item).hasClass('active')) {
+        index = i;
+        return;
+      }
+    })
+
+    var n = (isNext) ? (index + 1) % 3 : (index - 1) % 3;
+
+    items
+      .removeClass('active')
+      .eq(n).addClass('active')
+      ;
+  });
 
 })(jQuery); // End of use strict
